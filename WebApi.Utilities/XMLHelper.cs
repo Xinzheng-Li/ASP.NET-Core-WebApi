3071c9fac9c0916f35cff9c2258f91afbe913682
v-xinzli <v-xinzli@microsoft.com>
2020-06-04 14:07:59 +0800

    Add WebApi Utilities

---
+++ WebApi.Utilities/XMLHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml;
+
+namespace WebApi.Utilities
+{
+    public static class XMLHelper
+    {
+        /// <summary>
+        /// Get XmlNode By XPATH
+        /// </summary>
+        /// <param name="xmlFileName"></param>
+        /// <param name="xpath"></param>
+        /// <returns></returns>
+        public static XmlNode GetXmlNodeByXpath(string xmlFileName, string xpath)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.Load(xmlFileName);
+                XmlNode xmlNode = xmlDoc.SelectSingleNode(xpath);
+                return xmlNode;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
